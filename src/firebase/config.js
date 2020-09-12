ce2958f7ed49304057f1f52ac4f746995e899c1d
hiroki0816 <hiroki.oosugi@gmail.com>
2020-09-12 20:18:56 +0900

    create Cloud Functions API & get databese from firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVqqfkzwYLGuDh-jFtLu8kTdxCGw9L1Yc",
+    authDomain: "chatbot-demo-1cdc2.firebaseapp.com",
+    databaseURL: "https://chatbot-demo-1cdc2.firebaseio.com",
+    projectId: "chatbot-demo-1cdc2",
+    storageBucket: "chatbot-demo-1cdc2.appspot.com",
+    messagingSenderId: "410811847050",
+    appId: "1:410811847050:web:be9d17835480fdb85df873",
+    measurementId: "G-5FLD7WXK0W"
+  };
+
+  export default firebaseConfig
