b9461ebc01c665b363f46b671e354ead2f785389
hiroki0816 <hiroki.oosugi@gmail.com>
2020-09-12 11:11:48 +0900

    create chatbot logic

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,46 @@ export default class App extends React.Component  {
       dataset: defaultDataset,
       open: false
     }
+
+    this.selectAnswer =this.selectAnswer.bind(this)
   }
 
-  initAnswer = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const initAnswers = initDataset.answers;
-
-    this.setState( {answers: initAnswers})
+  displayNextQuestion = (nextQuestionId) => {
+      const chats = this.state.chats
+      chats.push({
+        text : this.state.dataset[nextQuestionId].question,
+        type : 'question'
+      })
+      this.setState({
+        answers : this.state.dataset[nextQuestionId].answers,
+        chats : chats,
+        currentId : nextQuestionId
+      })
   }
 
-  initChats = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const chat = {
-      text: initDataset.question,
-      type: 'question'
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId)
+        break;
+      default : 
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+    
+        this.setState( {chats: chats})
+
+        this.displayNextQuestion(nextQuestionId)
+        break;
     }
-
-    const chats = this.state.chats;
-    chats.push(chat)
-
-    this.setState( {chats: chats})
   }
+
 
   componentDidMount() {
-    this.initAnswer();
-    this.initChats()
+    const initAnswer = '';
+    this.selectAnswer(initAnswer, this.state.currentId);
   }
 
   render () {
@@ -45,7 +60,7 @@ export default class App extends React.Component  {
       <section className='c-section'>
         <div className='c-box'>
           <Chats chats={this.state.chats}/>
-          <AnswersList answers={this.state.answers} />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
         </div>
       </section>
     );
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import UserProfile from '../assets/img/user.png';
+import BotProfile from '../assets/img/chatbot.png';
 
 
 const Chat = (props) => {
+    // props.typeが'question'であればtrueを返す
     const isQuestion = (props.type === 'question');
+    // isQuestionがtrueの場合は左を返す falseの場合は右を返す
     const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse'
 
     return(
         <ListItem className={classes}>
             <ListItemAvatar>
-                <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+                {isQuestion ? (
+                    <Avatar alt="icon" src={BotProfile} />
+                ) : (
+                    <Avatar alt="icon" src={UserProfile} />
+                )}
             </ListItemAvatar>
             <div className='p-chat__bubble'>
                 {props.text}
